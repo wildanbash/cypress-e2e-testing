402448ef9ffe28c99e52d484741cce9f55e0ad66
bash <wildandawambash@gmail.com>
2020-10-20 19:44:25 +0700

    add using the great form test suite

--- cypress/integration/simple.spec.js
+++ cypress/integration/simple.spec.js
@@ -5,12 +5,23 @@ describe(' first test suite', () => {
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
     });
-    
+
     it('inline form test suite',() => {
         cy.get('input[placeholder="Jane Doe"]').type('Wildan Dawam Bash')
-        cy.get('[placeholder ="Email"][type="text"]').type('@gmail.com')
+        cy.get('[placeholder ="Email"][type="text"]').type('wildandawambash@gmail.com')
         cy.get('.custom-checkbox').first().click()
         cy.get('[status="primary"][type="submit"]')
         cy.contains('Submit')
     });
+
+    it('using the great form test suite',()=> {
+        cy.get('#inputEmail1').type('wildandawambash@gmail.com')
+        cy.get('#inputPassword2').type('12052000')
+        cy.get('.text').contains('Option 1').click()
+        cy.get('.text').contains('Option 2').click()
+        cy.get('[status="primary"][type="submit"]')
+        cy.contains('Sign in')
+    });
+
+    
 });
