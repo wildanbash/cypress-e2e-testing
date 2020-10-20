204ddeb05ebc7f5310ca5ffd743a6159935f05e3
bash <wildandawambash@gmail.com>
2020-10-20 19:22:31 +0700

    initial commit

---
+++ cypress/integration/simple.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+describe(' first test suite', () => {
+    it('visit form layout', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+    });
+
+    it('inline form test suite',() => {
+        cy.get('input[placeholder="Jane Doe"]').type('Wildan Dawam Bash')
+        cy.get('[placeholder ="Email"][type="text"]').type('wildandawambash@gmail.com')
+        cy.get('.custom-checkbox').first().click()
+        cy.get('[status="primary"][type="submit"]')
+        cy.contains('Submit')
+    });
+
+    
+});
